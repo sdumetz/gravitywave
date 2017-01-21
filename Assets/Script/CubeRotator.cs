63e90630e50ae118a85db59e97710c1d19986526
nicolasbremard <nicolas.bremard@inria.fr>
2017-01-21 12:07:32 +0100

    Add SphereGravity and Cube Rotator

---
+++ Assets/Script/CubeRotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            transform.Rotate(90,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Rotate(-90, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -90);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 90);
+        }
+
+    }
+}
--- Assets/Script/MouseMagnet.cs
+++ Assets/Script/MouseMagnet.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class MouseMagnet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+   private  Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Collision");
+            //transform.position = hit.point + new Vector3(0, GetComponent<Collider>().bounds.size.y / 2, 0);
+            if (rb)
+                rb.AddForce(hit.point - transform.position );
+        }
+    }
+
 }
