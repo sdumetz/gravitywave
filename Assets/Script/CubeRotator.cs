b1e4ef9da6578dde14d286ef20a0fff082779c83
nicolasbremard <nicolas.bremard@inria.fr>
2017-01-21 16:05:31 +0100

    Projet mis a jour

--- Assets/Script/CubeRotator.cs
+++ Assets/Script/CubeRotator.cs
@@ -4,28 +4,74 @@ using UnityEngine;
 
 public class CubeRotator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private bool animate = false;
+    private Vector3 rotationLeft;
+    public float speedAnimation = 0.02f;
+
+
+
+    // Use this for initialization
+    void Start () {
 
 
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.Rotate(90,0,0);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (!animate)
         {
-            transform.Rotate(-90, 0, 0);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                animate = true;
+                rotationLeft += transform.rotation * new Vector3(-90, 0, 0);
+
+                
+                //transform.Rotate(-90,0,0);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                animate = true;
+                rotationLeft += transform.rotation * new Vector3(90, 0, 0);
+
+
+                //transform.Rotate(90, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                animate = true;
+                rotationLeft += transform.rotation * new Vector3(0, 0, -90);
+
+
+                //transform.Rotate(0, 0, -90);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                animate = true;
+                rotationLeft += transform.rotation * new Vector3(0, 0, 90);
+
+                //transform.Rotate(0, 0, 90);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (animate)
         {
-            transform.Rotate(0, 0, -90);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.Rotate(0, 0, 90);
+
+            if (rotationLeft.magnitude < 1.0f)
+            {
+                transform.Rotate(rotationLeft);
+                animate = false;
+                rotationLeft = Vector2.zero;
+
+
+            }
+            else
+            {
+                Vector3 r = Vector3.Lerp(new Vector3(), rotationLeft, speedAnimation);
+                transform.Rotate(r);
+                rotationLeft -= r;
+
+
+            }
+
         }
 
     }
