b10a7dabd936043e85c5a799752a9746ca7b5554
Sebastien Dumetz <s.dumetz@holusion.com>
2017-01-21 21:41:21 +0100

    set central camera gameplay

---
+++ Assets/Script/CameraTrack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour {
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Sphere");
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target.transform, Vector3.up);
+    }
+}
--- Assets/Script/CubeRotator.cs
+++ Assets/Script/CubeRotator.cs
@@ -7,109 +7,45 @@ public class CubeRotator : MonoBehaviour {
     private bool animate = false;
     private Vector3 rotationLeft;
     public float speedAnimation = 0.02f;
-
-
+    public Space refspace;
+    public GameObject cam;
     //private Vector3 axis;
     //private float rest;
 
     // Use this for initialization
     void Start () {
-
-
+        refspace = Space.World;
     }
 
     // Update is called once per frame
     void Update() {
-        if (!animate)
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
         {
-            /*if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                animate = true;
-                //rotationLeft += transform.rotation * new Vector3(-90, 0, 0);
-                rotationLeft += new Vector3(90, 0, 0);
-
-                //axis = Vector3.right;
-                //rest = 90.0f;
-            }
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                animate = true;
-                //rotationLeft += transform.rotation * new Vector3(90, 0, 0);
-                rotationLeft +=  new Vector3(-90, 0, 0);
-
-
-                //axis = Vector3.right;
-                //rest = 90.0f;
-            }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                animate = true;
-                //rotationLeft += transform.rotation * new Vector3(0, 0, -90);
-                rotationLeft += new Vector3(0, 0, -90);
-
-
-                //axis = Vector3.forward;
-                //rest = 90.0f;
-            }
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                animate = true;
-                //rotationLeft += transform.rotation * new Vector3(0, 0, 90);
-                rotationLeft +=  new Vector3(0, 0, 90);
-
-                //axis = Vector3.right;
-                //rest = 90.0f;
-            }*/
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Rotate(new Vector3(1, 0, 0), Space.World);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Rotate(new Vector3(-1, 0, 0), Space.World);
+            transform.Rotate(new Vector3(1, 0, 0), refspace);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(-1, 0, 0), refspace);
                 
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Rotate(new Vector3(0, 0, -1), Space.World);
-
-
-                //axis = Vector3.forward;
-                //rest = 90.0f;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Rotate(new Vector3(0, 0, 1), Space.World);
-            }
         }
-        if (animate)
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            //if(rest < 1.0f)
-            if (rotationLeft.magnitude < 1.0f)
-            {
-                transform.Rotate(rotationLeft, Space.World);
-                animate = false;
-                rotationLeft = Vector2.zero;
-
-                //transform.Rotate(axis, rest);
-                //rest = 0.0f;
-
-            }
-            else
-            {
-                Vector3 r = Vector3.Lerp(new Vector3(), rotationLeft, speedAnimation);
-                transform.Rotate(r,Space.World);
-                rotationLeft -= r;
-
-                //float r = Mathf.Lerp(0.0f, rest, speedAnimation);
-                //float r = 1.0f;
-                //transform.Rotate(axis, rest);
-                //rest -= r;
-
-
-            }
-
+            transform.Rotate(new Vector3(0, 0, -1), refspace);
+            //axis = Vector3.forward;
+            //rest = 90.0f;
         }
-
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), refspace);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 1, 0), refspace);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0), refspace);
+        }
     }
 }
