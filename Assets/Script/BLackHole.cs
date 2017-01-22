46738126db7956aebbde3c2471ca45279503f3b2
Sebastien Dumetz <s.dumetz@holusion.com>
2017-01-22 16:44:32 +0100

    Final commit with particle system

--- Assets/Script/BLackHole.cs
+++ Assets/Script/BLackHole.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BLackHole : MonoBehaviour {
-
+    public GameObject target;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,6 @@ public class BLackHole : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(transform.up,6);
+        //transform.Rotate(target.transform.forward,6);
 	}
 }
