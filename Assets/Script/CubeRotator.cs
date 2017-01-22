b2b1fc5433487b1b1eb82369cdb8b26b0a2787f3
nicolasbremard <nicolas.bremard@inria.fr>
2017-01-22 13:56:54 +0100

    Gravity to point

--- Assets/Script/CubeRotator.cs
+++ Assets/Script/CubeRotator.cs
@@ -56,6 +56,6 @@ public class CubeRotator : MonoBehaviour {
         }
         //transform.localRotation = Quaternion.Euler(eulers);
 
-        Physics.gravity = gravityFactor * transform.forward;
+        //Physics.gravity = gravityFactor * transform.forward;
     }
 }
