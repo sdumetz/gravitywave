75f4cd8ccd81d531d43aa509420f0769ec7686b9
Sebastien Dumetz <s.dumetz@holusion.com>
2017-01-22 16:04:26 +0100

    Update black hole particles

---
+++ Assets/Script/BLackHole.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BLackHole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(transform.up,6);
+	}
+}
