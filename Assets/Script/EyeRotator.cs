09801269cdffe640d0c7f4d547a492d08040a767
nicolasbremard <nicolas.bremard@inria.fr>
2017-01-22 10:05:38 +0100

    Tobii modifications

--- Assets/Script/EyeRotator.cs
+++ Assets/Script/EyeRotator.cs
@@ -44,7 +44,7 @@ public class EyeRotator : MonoBehaviour {
             //watchingPos.y = Mathf.Pow(watchingPos.y, 3);
 
 
-            Vector3 rot = new Vector3( (watchingPos.y - Screen.height / 2.0f) / Screen.height, 0.0f, -1.0f * (watchingPos.x - Screen.width / 2.0f) / Screen.width);
+            Vector3 rot = new Vector3( -1.0f * (watchingPos.y - Screen.height / 2.0f) / Screen.height, 0.0f, 1.0f * (watchingPos.x - Screen.width / 2.0f) / Screen.width);
             //Debug.Log(rot);
             transform.Rotate(rot, Space.World);
         }
